8a8531358d7e7d57da45ad1cefbcc1c38e577aa1
Allagadda Sai Upendranath <sainath940@gmail.com>
2026-05-24 20:22:22 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+
+    mp := [26]int {}
+
+    for i := 0 ; i < len(s); i++ {
+        mp[s[i]-'a']++
+        mp[t[i]-'a']--
+    }
+
+    for _, val := range mp {
+        if val != 0 {
+            return false
+        }
+    }
+
+    return true
+}
